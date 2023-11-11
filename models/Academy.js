7c20aa68dd9b8d5ebfa5796c0e8240fd7fe9cb9c
alpha951 <20uec068@lnmiit.ac.in>
2023-11-11 22:56:06 +0530

    ADD timestapms

--- models/Academy.js
+++ models/Academy.js
@@ -1,26 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-
-const AcademySchema = new mongoose.Schema({
-
-    name:{
-        type: String,
-        required: [true, 'provide name'],
+const AcademySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "provide name"],
     },
-    sports:{
-        type:[String],
-        required:[true , 'enter atleast one sport'],
-        
+    sports: {
+      type: [String],
+      required: [true, "enter atleast one sport"],
     },
-    state : String,
-    city : String,
-    address : String,
+    state: String,
+    city: String,
+    address: String,
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide user'],
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user id who created this academy"],
     },
+  },
+  { timestamps: true }
+);
 
-});
-
-module.exports = mongoose.model('Academy', AcademySchema)
+module.exports = mongoose.model("Academy", AcademySchema);
