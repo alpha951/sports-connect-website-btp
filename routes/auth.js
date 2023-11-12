1f30367e76f8e53e91fe7c4b4474db5b14449c2e
alpha951 <20uec068@lnmiit.ac.in>
2023-11-12 16:47:18 +0530

    ADD middlewares added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/auth");
+const {
+  registerValidator,
+  loginValidator,
+} = require("../middleware/authentication");
+const { register, login } = require("../constorllers/auth");
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+router.route("/register").post(registerValidator, register);
+router.route("/login").post(loginValidator, login);
 
 module.exports = router;
