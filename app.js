5266f0170a53406b492709db85ca556e8e09d721
alpha951 <20uec068@lnmiit.ac.in>
2023-11-12 14:14:02 +0530

    ADD morgan package

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const PORT = process.env.PORT | 3000;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 const authRouter = require("./routes/auth");
 const players = require("./routes/routes");
 
 app.use("/auth", authRouter);
-app.use("/qwer", players);
+app.use("/play", players);
 
 app.all("*", function (req, res) {
-  res.send("not found");
+  res.status(404).send("not found");
 });
 
 mongoose
-  .connect(
-    // "mongodb+srv://mayank:mayank@cluster0.fy2cjib.mongodb.net/BTP?retryWrites=true&w=majority"
-    process.env.MONGO_URI
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("App connected to database");
     app.listen(PORT, function () {
