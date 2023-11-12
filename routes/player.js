f672238dc33f2c1bf7ed2d4dbfaf7cb521b71bf8
alpha951 <20uec068@lnmiit.ac.in>
2023-11-12 17:47:26 +0530

    UPDATE router export

--- routes/player.js
+++ routes/player.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { authValidator } = require("../middleware/authentication");
+const { eventValidator } = require("../middleware/event");
 const { getPlayers, updatePlayer } = require("../controllers/player");
 
 router.route("/getplayers").get(authValidator, getPlayers);
 router.route("/editprofile").patch(authValidator, updatePlayer);
-router.route("/create-event").post(authValidator, createEvent);
+router.route("/create-event").post(authValidator, eventValidator, createEvent);
+
+module.exports = router;
