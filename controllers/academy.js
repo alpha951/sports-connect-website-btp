d107e5fc6d862c6578e3f172842729d81ccf7a76
alpha951 <20uec068@lnmiit.ac.in>
2023-11-12 16:15:56 +0530

    UPDATE academy controller in separate file

---
+++ controllers/academy.js
@@ -0,0 +1,52 @@
+const Academy = require("../models/Academy");
+const User = require("../models/User");
+const asycnWrapper = require("../middleware/async");
+const { StatusCodes } = require("http-status-codes");
+const { haveCommonElements } = require("../utils/");
+
+const addAcademy = asycnWrapper(async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const academy = await Academy.create(req.body);
+  res.status(200).json({ academy });
+});
+
+const updateAcademy = asycnWrapper(async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.user;
+  delete req.body.createdBy;
+  const academy = await Academy.findByIdAndUpdate(
+    { _id: id, createdBy: userId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(StatusCodes.OK).json({ academy });
+});
+
+/*
+  This function is used to get academies based on the following criteria:
+   1. state
+   2. city
+   3. sports of interest
+*/
+const getAcademy = asycnWrapper(async (req, res, next) => {
+  const queryObject = {};
+  queryObject.state = req.body.state;
+  queryObject.city = req.body.city;
+  const academies = await Academy.find(queryObject);
+  let results = [];
+  for (let i = 0; i < academies.length; i++) {
+    if (haveCommonElements(academies[i].sports, req.body.sport)) {
+      results.push(academies[i]);
+    }
+  }
+  res.status(200).json(result);
+});
+
+module.exports = {
+  addAcademy,
+  updateAcademy,
+  getAcademy,
+};
