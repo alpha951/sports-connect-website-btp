1c6db4c06b39849322d6c5875ea3a21eb788194d
alpha951 <20uec068@lnmiit.ac.in>
2023-11-17 21:29:48 +0530

    Update default PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 5000;
 const { errorHandlerMiddleware } = require("./middleware/error-handler");
 const colors = require("@colors/colors");
 
